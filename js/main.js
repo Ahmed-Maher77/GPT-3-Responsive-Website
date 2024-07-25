98def74403e35ad763b8932b1b1e6e6516acd3ca
Ahmed Maher <ahmedmaheraljwhry057@gmail.com>
2024-07-25 19:09:02 +0300

    Add the main files, create the landing section

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+// Navbar
+const header = document.querySelector('#header');
+const navLinks = document.querySelectorAll('#header .nav-link');
+const navToggler = document.querySelector('#header .navbar-toggler');
+const togglerIcon = navToggler.querySelector('span');
+navLinks.forEach(link => {
+    link.onclick = function() {
+        navLinks.forEach(a => a.classList.remove('active'));
+        link.classList.add('active');
+    }
+})
+navToggler.onclick = function() {
+    header.classList.toggle('active');
+    togglerIcon.classList.toggle('fa-xmark')
+}
+// change the background when scroll
+window.onscroll = () => {
+    if (scrollY > 100) {
+        header.classList.add('scroll-bg')
+    } else {
+        header.classList.remove('scroll-bg')
+    }
+}
